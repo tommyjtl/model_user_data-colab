a83fbda85eafa6c0f70c95e646f4969a748ef101
tommyjtl <i@tjtl.io--global>
2021-02-09 23:11:52 +0800

    update

--- conf/start-train.sh
+++
@@ -1,1 +0,0 @@
-../tools/darknet-colab/darknet detector train processed-dataset.data processed-dataset.cfg darknet19_448.conv.23
--- conf/test-train.sh
+++
@@ -1,3 +0,0 @@
-cd /Users/tommyjtl/Documents/CocoRobo/Software/experiments/aws-cli-backend/toolkit/tools/darknet-colab
-./darknet detector test ../../conf/processed-dataset.data ../../conf/processed-dataset.cfg ../../backup/processed-dataset_last.weights ../../test/processed-dataset_test.jpg
-cd /Users/tommyjtl/Documents/CocoRobo/Software/experiments/aws-cli-backend/toolkit
