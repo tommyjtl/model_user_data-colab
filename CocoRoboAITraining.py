f2f930640919cc33043e0dac827d247cbb9556c4
tommyjtl <i@tjtl.io--global>
2021-02-09 23:30:51 +0800

    update

--- CocoRoboAITraining.py
+++ CocoRoboAITraining.py
@@ -133,20 +133,33 @@ class aws():
 
         os.chdir("../")
 
-    def run(self, command):
-        process = Popen(command, stdout=PIPE, shell=True)
+    def run_train_command(self, command):
+        process = subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE)
+        print("Process PID is: " + str(process.pid))
         while True:
-            line = process.stdout.readline().rstrip()
-            if not line:
+            output = process.stdout.readline()
+            if output == '' and process.poll() is not None:
                 break
-            yield line
+            if output:
+                formatted_output = output.strip().decode("utf-8")
+                if ("avg loss" in formatted_output) and ("rate" in formatted_output):
+                    print(formatted_output)
+                    iteration_times = int(formatted_output.split()[0][:-1])
+                    avg_loss = float(formatted_output.split()[2])
+                    # print(str(int(iteration_times)) + "," + str(float(formatted_output.split()[2])))
+                    if avg_loss < 0.06: break
+        # process.terminate()
+        process.terminate()
+        try:
+            process.wait(timeout=0.2)
+            print('== subprocess exited with rc =', process.returncode)
+        except subprocess.TimeoutExpired:
+            print('subprocess did not terminate in time')
+        os.kill(int(process.pid)+1, signal.SIGKILL)
 
     def train(self):
         os.chdir("./toolkit/conf/")
-
-        for path in self.run("python train.py"):
-            print(path)
-
+        self.run_train_command("bash start-train.sh")
         os.chdir("../../")
 
     def validate(self):
